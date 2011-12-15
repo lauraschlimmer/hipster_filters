82f2839c31d9565a0337d6ae001983ec949ea26b
Paul Asmuth <paul@23linesofcode.com>
2011-12-15 22:09:45 +0100

    blackwhite processor done

--- filterfnord.rb
+++ filterfnord.rb
@@ -117,11 +117,43 @@ class LensFlareProcessor < ProcessableImage
     )
 
     # screen the lensflare into our target image
-
     cmd_compose(
       "#{helper_file(:lfscreen)} #{current_source_file}",
-      :compose => "Screen", 
-      :gravity => "NorthWest"
+      :compose => "screen", 
+      :gravity => "northwest"
+    )
+
+    # done :)
+    incr_compositing_step
+    super
+
+  end
+
+end
+
+class BlackWhiteProcessor < ProcessableImage
+
+  def process!
+    
+    # make the target image black and white
+    cmd_convert(
+      :type => "grayscale"
+    )
+
+    incr_compositing_step
+
+    # scale the gradient to our target size
+    cmd_convert(
+      asset_file('bwgrad_1.png'), 
+      helper_file(:bwgrad), 
+      :resize => "#{width}x#{height}!"
+    )
+
+    # multiply the gradient into our target image
+    cmd_compose(
+      "#{helper_file(:bwgrad)} #{current_source_file}",
+      :compose => "softlight", 
+      :gravity => "center"
     )
 
     # done :)
@@ -133,7 +165,7 @@ class LensFlareProcessor < ProcessableImage
 end
 
 
-
-res = LensFlareProcessor.new('/home/paul/Code/filterfnord/test2.jpg').process!
+res = LensFlareProcessor.new('/home/paul/Code/filterfnord/test1.png').process!
+#res = BlackWhiteProcessor.new('/home/paul/Code/filterfnord/test1.png').process!
 puts "DONE!: #{res}"
 %x{gnome-open #{res} &> /dev/null}
